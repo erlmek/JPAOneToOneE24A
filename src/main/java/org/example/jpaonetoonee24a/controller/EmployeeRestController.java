c4c0d949e66d0b2c077972bb3615541c4136c71a
erlmek <erlm@ek.dk>
2025-09-02 11:17:20 +0200

    post employee

--- src/main/java/org/example/jpaonetoonee24a/controller/EmployeeRestController.java
+++ src/main/java/org/example/jpaonetoonee24a/controller/EmployeeRestController.java
@@ -1,10 +1,13 @@
 package org.example.jpaonetoonee24a.controller;
 
 import org.example.jpaonetoonee24a.model.Employee;
+import org.example.jpaonetoonee24a.model.User;
 import org.example.jpaonetoonee24a.repository.EmployeeRepository;
+import org.example.jpaonetoonee24a.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +17,23 @@ public class EmployeeRestController {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
+
+    @PostMapping("employee")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addEmployee(@RequestBody Employee employee) {
+        User usr = employee.getUser();
+        usr = userRepository.save(usr);
+        employee.setUser(usr);
+        return employeeRepository.save(employee);
+    }
+
 
 }
